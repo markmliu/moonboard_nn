96f5c8c420c3e1c8855e59421e5ef7a412770837
Mark Liu <uilkram@gmail.com>
2022-08-02 15:10:14 -0700

    move more to utils and allow prefix adding

--- names/utils.py
+++ names/utils.py
@@ -8,6 +8,15 @@ import os
 import time
 import operator
 
+from keras_preprocessing.sequence import pad_sequences
+import numpy as np
+
+def valid_char(ch):
+    return ch >= 'A' and ch <= 'Z' or ch.isspace() or ch >= '0' and ch <= '9'
+
+def valid_string(name):
+    return all([valid_char(ch) for ch in name])
+
 # for some reason these two functions are way faster than the keras char-level tokenizer
 def map_char_to_int(texts,labels):
     char_counts = {}
@@ -101,3 +110,40 @@ class SaveHistoryCheckpoint(keras.callbacks.Callback):
         #self.history['peak_memory'] = get_memory()
         save_history_file(self.model_path, self.history)
         save_training_plots(self.model_path)
+
+def convert_problem_to_input(holds):
+    # encode each problem as csv list of holds, followed by space, then name of the problem
+    inp = []
+    hold_string = ','.join([hold['Description'] for hold in holds])
+    hold_string += ' '
+    return hold_string
+
+def sample_from_prob_vec(pr):
+    # sample from output of model (ignoring very rare outcomes)
+    prob_thresh = np.max(pr,1) * 0.1
+    pr[pr < prob_thresh]=0
+    pr = (pr/pr.sum(axis=1,keepdims=1))[0]
+    idxs = np.arange(len(pr))
+    return np.random.choice(idxs, 1, p=pr)[0]
+
+# Take "hold text (like 'A5,B8,D12,F14,I18 ' or output of `convert_problem_to_input` and output a name)
+def name_text(text, model, char_to_int, int_to_char, max_length, end_token, prefix=None):
+    if prefix is not None:
+        prefix = prefix.upper()
+        if valid_string(prefix):
+            aug_text = text+prefix
+    texts = [aug_text]
+    #single
+    # for each text, continue predicting until we reach max length or end token
+    final_texts = texts
+    while True:
+
+        sequences = texts_to_sequences(final_texts, char_to_int)
+        data = pad_sequences(sequences, maxlen=max_length)
+        predictions_list = model.predict(data)
+
+        ch = sample_from_prob_vec(predictions_list)
+        if len(sequences[0]) > max_length or ch == end_token:
+            break
+        final_texts[0]+=int_to_char[ch]
+    return final_texts[0][len(text):]
